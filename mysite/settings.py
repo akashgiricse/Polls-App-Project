692b9231b31f5e58cc6e1d2b4fe520a45b3e6660
akashgiricse <akashgiricse@gmail.com>
2017-11-29 01:54:34 +0530

    Changed Debug to False for security purpose:

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=taz(%o!n#(0(%rg=-a8yycd#f=!@m1bzsb88mu+x-7l5x3ote'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['akashpollapp.pythonanywhere.com']
 
